40b379d80298a639739fff142666f598582e4717
Emily Chapman <chapmae3@gmatc.matc.edu>
2022-09-10 15:50:31 -0500

    Added database connection

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import connectDatabase from './config/db';
 
 //Initialize express application
 const app = express();
+
+//Connect to Database
+connectDatabase();
 
 //Create API endpoints
 app.get('/', (req, res) => 
