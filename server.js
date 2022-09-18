a0cfc68df14287868fc659ba6a9ed25560d63ac4
Emily Chapman <chapmae3@gmatc.matc.edu>
2022-09-18 16:26:07 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import {check, validationResult} from 'express-validator';
 
 //Initialize express application
 const app = express();
@@ -7,9 +8,35 @@ const app = express();
 //Connect to Database
 connectDatabase();
 
+//Configure Middleware
+app.use(express.json({ extended: false }));
+
 //Create API endpoints
+
 app.get('/', (req, res) => 
     res.send('http get request sent to root api endpoint')
+);
+
+app.post(
+    '/api/users',
+    [
+        check('name', 'Please enter your name.')
+            .not()
+            .isEmpty(),
+        check('email', 'Please enter a valid email.')
+            .isEmail(),
+        check('password', 'Please enter a password with six or more characters.')
+            .isLength({min: 6})
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array()});
+        }
+        else {
+            return res.send(req.body);
+        }
+    }
 );
 
 //Set up connection listener
