e74a0f11ce7132c2f3c2f8dccecd76a811bd1fc1
Emily Chapman <chapmae3@gmatc.matc.edu>
2022-09-24 16:40:36 -0500

    Completed Activity 4

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDatabase from './config/db';
 import {check, validationResult} from 'express-validator';
+import cors from 'cors';
 
 //Initialize express application
 const app = express();
@@ -10,9 +11,13 @@ connectDatabase();
 
 //Configure Middleware
 app.use(express.json({ extended: false }));
+app.use(
+    cors({
+        origin: 'http://localhost:3000'
+    })
+);
 
 //Create API endpoints
-
 app.get('/', (req, res) => 
     res.send('http get request sent to root api endpoint')
 );
@@ -40,4 +45,5 @@ app.post(
 );
 
 //Set up connection listener
-app.listen(3000, () => console.log('Express server running on port 3000'));
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
