e8e890cd232bcd723ef2f51fe1f05f18d58e3fce
Emily Chapman <chapmae3@gmatc.matc.edu>
2022-11-05 11:35:17 -0500

    Created the Delete Post API Endpoint

--- server.js
+++ server.js
@@ -184,10 +184,33 @@ app.get('/api/posts/:id', auth, async (req, res) => {
 
         //Make sure the post was found
         if (!post) {
-            res.status(400).json({msg: 'Post not found.'});
+            return res.status(400).json({msg: 'Post not found.'});
         }
 
         res.json(post);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error.');
+    }
+});
+
+app.delete('/api/posts/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //Make sure the post was found
+        if (!post) {
+            return res.status(400).json({msg: 'Post not found.'});
+        }
+
+        //Make sure the request user created the post
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({msg: 'User not authorized.'});
+        } 
+
+        await post.remove();
+
+        res.json({msg: 'Post removed.'});
     } catch (error) {
         console.error(error);
         res.status(500).send('Server error.');
