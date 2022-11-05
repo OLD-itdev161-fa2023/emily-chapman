35441a07779d25560021f5acfd23a1e07ee63a5a
Emily Chapman <chapmae3@gmatc.matc.edu>
2022-11-05 12:00:44 -0500

    Completed Activity 9

--- server.js
+++ server.js
@@ -219,7 +219,7 @@ app.delete('/api/posts/:id', auth, async (req, res) => {
 
 app.put('/api/posts/:id', auth, async (req, res) => {
     try {
-        const {title, body} = req.body;
+        const { title, body } = req.body;
         const post = await Post.findById(req.params.id);
 
         //Make sure the post was found
